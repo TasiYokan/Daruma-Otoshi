8af3459c0a09189b5e1d03d49995db6294033b59
TasiYokan <guanghao.yang@outlook.com>
2017-12-04 00:11:48 -0500

    Add a hammer

--- Assets/Scripts/DragAndHit.cs
+++ Assets/Scripts/DragAndHit.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DragAndHit : MonoBehaviour
 {
     private Vector3 startPos;
+    public SpriteRenderer hammerSprite;
+    private bool m_isMoving = false;
 
     // Use this for initialization
     void Start()
@@ -28,18 +31,46 @@ public class DragAndHit : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             Debug.DrawLine(startPos, Camera.main.ScreenToWorldPoint(Input.mousePosition), Color.red);
+            hammerSprite.enabled = true;
+            hammerSprite.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition).SetZ(0);
+        }
+        else if (m_isMoving == false)
+        {
+            hammerSprite.enabled = false;
         }
     }
 
     private void FindTargetBlock()
     {
-		Vector3 forceDir = startPos - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 forceDir = startPos - Camera.main.ScreenToWorldPoint(Input.mousePosition);
         RaycastHit2D hit = Physics2D.Raycast(startPos, forceDir);
         // Debug.DrawLine(startPos, Camera.main.ScreenToWorldPoint(Input.mousePosition));
         if (hit.collider != null && hit.transform.CompareTag("Block"))
         {
             print("hit " + hit.transform.name);
-			hit.transform.GetComponent<Rigidbody2D>().AddForce(forceDir * 3, ForceMode2D.Impulse);
+
+            StartCoroutine(SwapHammerTo(hit.point - Vector2.left * 0.85f, () =>
+              {
+                  hit.transform.GetComponent<Rigidbody2D>().AddForce(forceDir * 3, ForceMode2D.Impulse);
+              }));
         }
+    }
+
+    private IEnumerator SwapHammerTo(Vector3 _targetPos, Action _callback)
+    {
+        m_isMoving = true;
+        while ((hammerSprite.transform.position - _targetPos).SetZ(0).magnitude > 0.1f)
+        {
+            Vector3 dis = (_targetPos - hammerSprite.transform.position).SetZ(0);
+            print("hammer pos " + hammerSprite.transform.position);
+            print("dis " + dis.magnitude);
+            hammerSprite.transform.Translate(dis * 0.5f);
+
+            yield return null;
+        }
+
+        m_isMoving = false;
+        if (_callback != null)
+            _callback();
     }
 }
