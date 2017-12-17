3e56718b52f0f7430394fa838c4e822882750f66
TasiYokan <guanghao.yang@outlook.com>
2017-12-17 00:47:21 -0500

    Try to use contact info to detect impact. Need more tests

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public enum BlockType
@@ -13,8 +14,33 @@ public class Block : MonoBehaviour
     private bool hasBeenMarkedDestroyed = false;
     public BlockType blockType;
 
-    public bool isStable = true;
+    [SerializeField]
+    private bool m_isStable = true;
     public BoundsCollisionDetector colDetector;
+
+    ContactPoint2D[] contacts = new ContactPoint2D[10];
+    Rigidbody2D rig;
+
+    private bool m_isPlayingImpactAnim = false;
+    private int m_remainImpactTime;
+    public GameObject impactPlaceHolder;
+
+    public bool IsStable
+    {
+        get
+        {
+            return m_isStable;
+        }
+
+        set
+        {
+            if (value != m_isStable)
+            {
+                //print(name + " change to stable " + value + " with impact speed " + rig.velocity);
+            }
+            m_isStable = value;
+        }
+    }
 
     // Use this for initialization
     void Start()
@@ -23,8 +49,8 @@ public class Block : MonoBehaviour
         {
             colDetector.onContactWithOneSide = (_trans) =>
             {
-                if (_trans.position.y < transform.position.y)
-                    print(name + " Land on " + _trans.name);
+                //if (_trans.position.y < transform.position.y)
+                //    print(name + " Land on " + _trans.name);
             };
 
             colDetector.onExitContactWithOneSide = (_trans) =>
@@ -33,6 +59,7 @@ public class Block : MonoBehaviour
                 //    print("No longer land on " + _trans.name);
             };
         }
+        rig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -43,6 +70,74 @@ public class Block : MonoBehaviour
             //print("Fall from desk " + name);
             StartCoroutine(DestroyGameobject());
         }
+        CheckContacts();
+    }
+
+    private void CheckContacts()
+    {
+        int contactNum = rig.GetContacts(contacts);
+        if (contactNum < 2)
+        {
+            IsStable = false;
+            return;
+        }
+
+        for (int i = 0; i < contacts.Length; ++i)
+        {
+            // If the contacting rigidbody is under this transform
+            if (contacts[i].rigidbody != null
+                && contacts[i].rigidbody.position.y < transform.position.y)
+            {
+                // Search if there's another step point
+                for (int j = i + 1; j < contacts.Length; ++j)
+                {
+                    if (contacts[j].rigidbody != null
+                        && contacts[j].rigidbody == contacts[i].rigidbody)
+                    {
+                        if (IsStable == false)
+                        {
+                            float impact =
+                                (contacts[j].relativeVelocity * contacts[j].normalImpulse + contacts[i].relativeVelocity * contacts[i].normalImpulse).magnitude;
+                            if (impact > 9f)
+                            {
+                                print(name + " get impluse " +
+                                     impact + " from " + contacts[i].rigidbody.name);
+
+                                if(m_isPlayingImpactAnim == false)
+                                {
+                                    StartCoroutine(PlayImpactAnim());
+                                }
+                                else
+                                {
+                                    m_remainImpactTime += 20;
+                                }
+                            }
+                            IsStable = true;
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+        IsStable = false;
+        return;
+    }
+
+    private IEnumerator PlayImpactAnim()
+    {
+        m_isPlayingImpactAnim = true;
+        m_remainImpactTime = 100;
+        impactPlaceHolder.SetActive(true);
+        while (m_remainImpactTime > 0)
+        {
+            m_remainImpactTime--;
+
+            yield return null;
+        }
+
+        impactPlaceHolder.SetActive(false);
+        print("Impact finished");
+        m_isPlayingImpactAnim = false;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -53,11 +148,11 @@ public class Block : MonoBehaviour
             StartCoroutine(DestroyGameobject());
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.transform.position.y < transform.position.y)
-            isStable = false;
-    }
+    //private void OnTriggerExit2D(Collider2D collision)
+    //{
+    //    if (collision.transform.position.y < transform.position.y)
+    //        isStable = false;
+    //}
 
     private IEnumerator DestroyGameobject()
     {
